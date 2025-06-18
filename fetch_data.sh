303f07953ffdbc69d711e72953e5c43b2389d54c
youting8914 <etyouting@gmail.com>
2025-06-19 02:16:56 +0800

    ci: use 7z to extract .rar in fetch_data.sh

--- fetch_data.sh
+++ fetch_data.sh
@@ -4,13 +4,13 @@ set -e
 declare -A files=(
   [rotmod_files]="https://zenodo.org/records/15691753/files/rotmod_files.rar?download=1"
   [DH_data]="https://zenodo.org/api/records/15691765/draft/files/DH_data.rar/content"
-
-  # 如果后续还有，继续在这里添加 key=url
 )
 
 for dir in "${!files[@]}"; do
   url="${files[$dir]}"
   mkdir -p "$dir"
-  wget -O "$dir/${dir}.zip" "$url"
-  unzip -o "$dir/${dir}.zip" -d "$dir"
+  # 下载为 .rar
+  wget -O "$dir/${dir}.rar" "$url"
+  # 用 7z 解压所有内容到对应目录
+  7z x "$dir/${dir}.rar" -o"$dir"
 done
