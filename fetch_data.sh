b8599dd355f8c38910b4f36179f950ccf1d4b771
youting8914 <etyouting@gmail.com>
2025-06-19 11:41:18 +0800

    ci: use public Zenodo link for DH_data

--- fetch_data.sh
+++ fetch_data.sh
@@ -3,7 +3,7 @@ set -e
 
 declare -A files=(
   [rotmod_files]="https://zenodo.org/records/15691753/files/rotmod_files.rar?download=1"
-  [DH_data]="https://zenodo.org/api/records/15691765/draft/files/DH_data.rar/content"
+  [DH_data]="https://zenodo.org/records/15691765/files/DH_data.rar?download=1"
 )
 
 for dir in "${!files[@]}"; do
